2b37d4d028c0f2e4d93b484bf02d7ff59d7b5b4e
Arleensingh <87755058+Arleensingh@users.noreply.github.com>
2022-10-20 16:36:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+noseX = 0;
+noseY = 0;
+
+function modelLoaded() {
+    console.log("PoseNet is Initiated");
+}
+
+function gotPoses(results) {
+    console.log(results);
+    if (results.length>0) {
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
+
+function draw() {
+    image (video, 0, 0, 300, 300);
+    fill("red");
+    stroke("black");
+    circle(noseX, noseY, 20);
+}
